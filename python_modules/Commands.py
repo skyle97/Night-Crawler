08f6f0963da8581b1010cc3abbacc520ed0c03d3
intrackeable <olesh_@hotmail.com>
2021-10-22 10:28:35 -0300

    CLI with Argparse

---
+++ python_modules/Commands.py
@@ -0,0 +1,23 @@
+import argparse
+
+
+class Commands:
+    def get_flags():
+        parser = argparse.ArgumentParser(description="Night Crawler")
+        parser.add_argument("-s",
+                            "--start",
+                            type=str,
+                            dest="start_ip")
+
+        parser.add_argument("-e",
+                            "--end",
+                            type=str,
+                            dest="end_ip")
+
+        parser.add_argument("-t",
+                            "--threads",
+                            type=int,
+                            dest="threads")
+
+        flags = parser.parse_args()
+        return flags.start_ip, flags.end_ip, flags.threads
--- python_modules/main.py
+++ python_modules/main.py
@@ -1,8 +1,16 @@
 from Crawler import Crawler
+from Commands import Commands
+
 
 def main():
-    NightCrawler = Crawler("186.182.0.0","186.182.255.255",100)
-    NightCrawler.start_threads()
+    start, end, threads = Commands.get_flags()
+    if (start and end and threads):
+        NightCrawler = Crawler(start, end, threads)
+        NightCrawler.start_threads()
+    else:
+        print("Please use -h to see all options")
+        exit()
+
 
 if __name__ == '__main__':
     try:
