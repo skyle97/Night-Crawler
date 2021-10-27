b758fa86b00aed1b90ca8da945bac6a8ad8bd407
intrackeable <olesh_@hotmail.com>
2021-10-27 14:54:46 -0300

    Deleting

--- api/routes/devices.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var device_controller = require('../controllers/controller');
-var router = express.Router();
-
-router.get('/search',device_controller.search);
-module.exports = router;
-
-
