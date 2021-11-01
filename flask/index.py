279cf5c2a6bb191d689d78d93220408e3d059ce7
intrackeable <olesh_@hotmail.com>
2021-11-01 13:07:50 -0300

    New backend paths, get documents by IP/ID and total count

--- flask/index.py
+++ flask/index.py
@@ -1,68 +1,41 @@
+from typing import Collection
+from dns.flags import DO
 from flask import Flask, json, jsonify, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
-from bson import ObjectId
-import re
+from pymongo import database
+from Document import Document
 
 app = Flask(__name__)
 app.config['MONGO_URI'] = "mongodb://localhost:27017/IOT"
 mongo = PyMongo(app)
 CORS(app)
 
-db = mongo.db.devices
-
+database = mongo.db.devices
+Collection = Document(database)
 
 @app.route("/", methods=['GET'])
 def home():
     return "Hello World!"
 
-
-@app.route("/search/<new_query>", methods=['GET'])
-def get_devices(new_query):
-    query = new_query.lower().split("=")
-    if (query[1] != ""):
-
-        if (query[0] == "port"):
-            collections = db.find({"ports": int(query[1])})
-
-        elif (query[0] == "service"):
-            collections = db.find({"services": str(query[1])})
-
-        elif (query[0] == "banner"):
-            reg = re.compile(r"{}".format(query[1]), re.I)
-            collections = db.find({"banners": {'$regex': reg}})
-        else:
-            return "Invalid search filter"
-        devices = []
-
-        for col in collections:
-            colect = ({
-                'ip': col['ip'],
-                'ports': col['ports'],
-                'services': col['services'],
-                'banners': col['banners'],
-                'date': col['date'],
-                'geo': col['geo']
-            })
-            devices.append(colect)
-        return jsonify(devices)
-    else:
-        return "Invalid search filter"
-
+@app.route("/search/<query>", methods=['GET'])
+def search(query):
+    return Collection.get_devices(query)
 
 @app.route("/search/id/<id>", methods=['GET'])
-def get_one_device(id):
-    device = db.find_one({'_id': ObjectId(id)})
-    return jsonify({
-        '_id': str(ObjectId(device['_id'])),
-        'ip': device['ip'],
-        'ports': device['ports'],
-        'services': device['services'],
-        'banners': device['banners'],
-        'date': device['date'],
-        'geo': device['geo']
-    })
+def get_id(id):
+    return Collection.get_document_id(id)
 
+
+@app.route("/search/ip/<ip>", methods=['GET'])
+def get_ip(ip):
+    return Collection.get_document_ip(ip)
+
+
+@app.route("/search/total", methods=['GET'])
+def get_count():
+    return str(database.count())
 
 if __name__ == '__main__':
     app.run(debug=True)
+
