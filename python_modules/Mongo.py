76cac6d7f31d84062c0769a053e47753d5b2de75
intrackeable <olesh_@hotmail.com>
2021-11-02 16:33:24 -0300

    Some changes in json response

--- python_modules/Mongo.py
+++ python_modules/Mongo.py
@@ -3,20 +3,26 @@ import pymongo
 import json
 import requests
 
+
 class Mongo:
-    def insert_document(self,ip,ports,services,banners,image_path):
-        client = pymongo.MongoClient("localhost",27017)
+    def insert_document(self, ip, ports, services, banners, image_path):
+        client = pymongo.MongoClient("localhost", 27017)
         self.db = client['IOT']
         self.devices_collection = self.db['devices']
-        self.devices_collection.insert({"ip":ip,"ports":ports,"services":services,"banners":banners,"date":self.get_time(), "geo":self.geo_ip(ip), "screenshot":image_path})
+        self.geo = self.geo_ip(ip)
+        self.devices_collection.insert({"ip": ip, "banners": banners, "services": services, "ports": ports, "country": self.geo[0], "region_name": self.geo[1], "city": self.geo[2],
+                                        "country_code": self.geo[3], "zip_code": self.geo[4], "time_zone": self.geo[5], "latitude": self.geo[6], "longitude": self.geo[7], "date": self.get_time(), "screenshot": image_path})
 
     def get_time(self):
         current = datetime.now()
-        return current.strftime("%d/%m/%Y %H:%M:%S")
+        return current.strftime("%d/%m/%Y %H:%M")
 
-    def geo_ip(self,ip):
-        response = requests.get("https://geolocation-db.com/jsonp/{}".format(ip))
+    def geo_ip(self, ip):
+        document = []
+        API_KEY = ""
+        response = requests.get("https://api.freegeoip.app/json/{}?apikey={}".format(ip, API_KEY))
         result = response.content.decode()
-        result = result.split("(")[1].strip(")")
-        return json.loads(result)
-    
+        res = json.loads(result)
+        document = [res['country_name'], res['region_name'], res['city'], res['country_code'],
+                    res['zip_code'], res['time_zone'], res['latitude'], res['longitude']]
+        return document
