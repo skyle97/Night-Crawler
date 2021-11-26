77085031b94db0f25093340c0b06e3df6401f26b
intrackeable <olesh_@hotmail.com>
2021-11-26 11:11:52 -0300

    Boolean flags in collections

--- scanner/Mongo.py
+++ scanner/Mongo.py
@@ -24,6 +24,7 @@ class Mongo:
             return True
 
     def insert_document(self):
+        #Add boolean flags in collections, to determine if has been performed a dictionary attack
         if self.validate("ftp"):
             self.collection["ftp_flag"] = False
 
