bb595cc70590909469d6c20face728b63a2c99ca
intrackeable <olesh_@hotmail.com>
2021-11-28 23:31:34 -0300

    Rename class to Thread Scanner

--- scanner/ThreadScanner.py
+++ scanner/ThreadScanner.py
@@ -3,35 +3,37 @@ from numpy import array_split
 from ipaddress import ip_address
 from NetworkScanner import NetworkScanner
 from Mongo import Mongo
-from colorama import Fore
+from loguru import logger
 
-class Crawler:
+import sys
+
+class ThreadScanner:
     def __init__(self,start,end,threads):
-        self.start = start
-        self.end = end
-        self.threads = threads
-        self.ip_list = array_split(self.get_ranges(),self.threads)
+        self.ip_list = array_split(self.get_ranges(start,end),threads)
        
-    def get_ranges(self):
-        start_int = int(ip_address(self.start).packed.hex(), 16)
-        end_int = int(ip_address(self.end).packed.hex(), 16)
+    def get_ranges(self,start,end):
+        #Get total of ip addresses
+        start_int = int(ip_address(start).packed.hex(), 16)
+        end_int = int(ip_address(end).packed.hex(), 16)
         return [ip_address(ip).exploded for ip in range(start_int, end_int)]
 
-    def new_worker(self,sub_list,count):
+    def job(self,sub_list):
         for ip in sub_list:
             Scanner = NetworkScanner(ip)
-            Scanner.start_scanner()
+            Scanner.start()
             if Scanner.get_ports():
-                ip, banners, ports, services, hostname = Scanner.get_results()
-                image = Scanner.get_image()
+                ip, banners, ports, services, hostname, image = Scanner.get_results()
                 Base = Mongo()
                 Base.create_document(ip, ports, services,banners, hostname, image)
                 Base.insert_document()
-                print(Fore.GREEN + Base.show_document(count)+Fore.RESET)
+                logger.success(Base.show_document())
 
     def start_threads(self):
-        count = 0
-        for sub in self.ip_list:
-            count += 1
-            worker = threading.Thread(target=self.new_worker, args=(sub, count))
-            worker.start()
+        try:
+            for sub in self.ip_list:
+                thread = threading.Thread(target=self.job, args=(sub,))
+                thread.start()
+        except KeyboardInterrupt:
+            #Not working, fix this issue later
+            logger.info("You pressed CTRL+C")
+            sys.exit(1)
