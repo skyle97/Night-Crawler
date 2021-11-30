2032805cf6ccde3e2024dcd78d34bebf8e1b980a
intrackeable <olesh_@hotmail.com>
2021-11-29 23:39:05 -0300

    New scanner options

--- scanner/Commands.py
+++ scanner/Commands.py
@@ -1,5 +1,5 @@
 import argparse
-
+import pathlib
 
 class Commands:
     def get_flags():
@@ -18,7 +18,24 @@ class Commands:
         parser.add_argument("-t",
                             "--threads",
                             type=int,
-                            dest="threads")
+                            dest="threads",
+                            default=100)
 
+        parser.add_argument("-m",
+                            "--massive-scan",
+                            type=pathlib.Path,
+                            help="File path with IP blocks",
+                            dest="file")
+
+        parser.add_argument("--timeout",
+                            type=int,
+                            help="Socket time out",
+                            dest="timeout",
+                            default=1)
+
+        parser.add_argument("--screenshot",
+                            action="store_true",
+                            help="Take screenshots from devices with HTTP")
+        
         flags = parser.parse_args()
-        return flags.start_ip, flags.end_ip, flags.threads
+        return flags.start_ip, flags.end_ip, flags.threads,flags.file, flags.timeout, flags.screenshot
