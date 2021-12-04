74b9dc69047c59d32b762e21cc3cf52a35dd5e2e
intrackeable <olesh_@hotmail.com>
2021-12-04 01:32:30 -0300

    Implementing Queue to limit number of threads, fixed general issues

--- scanner/ThreadScanner.py
+++ scanner/ThreadScanner.py
@@ -1,18 +1,22 @@
-import threading
-from numpy import array_split
-from ipaddress import ip_address
+#Local modules
 from NetworkScanner import Network_Scanner
 from Mongo import Mongo
+
+from ipaddress import ip_address
 from loguru import logger
 from datetime import datetime
+from time import sleep
 
+import queue
 import sys
+import threading
 
 class Thread_Scanner():
     def __init__(self,start,end,threads,timeout,screenshot):
         self.timeout = timeout
         self.screenshot = screenshot
-        self.ip_list = array_split(self.get_ranges(start,end),threads)
+        self.ip_list = self.get_ranges(start,end)
+        self.threads = threads
 
     def get_ranges(self,start,end):
         #Get total of ip addresses
@@ -20,34 +24,33 @@ class Thread_Scanner():
         end_int = int(ip_address(end).packed.hex(), 16)
         return [ip_address(ip).exploded for ip in range(start_int, end_int)]
 
-    def job(self,sub_list):
-        for ip in sub_list:
+    def job(self,q):
+        while not q.empty():
+            ip = q.get()
             Scanner = Network_Scanner(ip,self.timeout,self.screenshot)
             Scanner.start()
-            if Scanner.get_ports():
-                ip, banners, ports, services, hostname, image = Scanner.get_results()
-                Base = Mongo()
-                Base.create_document(ip, ports, services,banners, hostname, image)
-                Base.insert_document()
-                logger.success(Base.show_document())
-        
+            q.task_done()
+
     def start_threads(self):
+        #Implemeting Queue to limit number of threads
         logger.info("Searching connected devices, please wait")
-        jobs = []
         start = datetime.now()
+        q = queue.Queue()
         try:
             logger.info("Launching threads")
-            for sub in self.ip_list:
-                thread = threading.Thread(target=self.job, args=(sub,))
+            for j in self.ip_list:
+                q.put(j)
+                
+            logger.info("Waiting for Queue to complete, {} jobs".format(q.qsize()))
+        
+            for i in range(self.threads):
+                thread = threading.Thread(target=self.job, args=(q,),daemon=True)
                 thread.start()
-                jobs.append(thread)
-            for  t in jobs:
-                t.join()
+            q.join()
             end = datetime.now()
-            elapsed_time = end-start
-            logger.info("Execution time: {}".format(elapsed_time))
-            
+            elapsed = end-start
+            logger.info("Execution time: {}".format(elapsed))
+
         except KeyboardInterrupt:
-            #Not working, fix this issue later
             logger.info("You pressed CTRL+C")
             sys.exit(1)
