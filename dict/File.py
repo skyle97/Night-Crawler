6b9d2169fcfb0e62e04688b84803b2df8b5aed40
intrackeable <olesh_@hotmail.com>
2021-12-08 15:18:37 -0300

    Update

--- dict/File.py
+++
@@ -1,5 +0,0 @@
-def open_file(file_list):
-    with open(file_list, 'r') as file:
-        credentials = file.read().split('\n')
-    credentials = [x for x in credentials if x != '']
-    return credentials
--- dict/main.py
+++
@@ -1,7 +0,0 @@
-from ftp import FTP_Test
-def main():
-    test = FTP_Test("../default-credentials/ftp.txt")
-    test.start()
-
-if __name__ == '__main__':
-    main()
