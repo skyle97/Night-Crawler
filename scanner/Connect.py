d8efd85e15109e2a5677195e289c7d78e806b8bd
intrackeable <olesh_@hotmail.com>
2021-12-22 15:48:54 -0300

    Elasticsearch connection

--- scanner/Connect.py
+++ scanner/Connect.py
@@ -1,7 +1,3 @@
-import pymongo
+from elasticsearch import Elasticsearch
 
-def database_connect():
-    client = pymongo.MongoClient("localhost", 27017)
-    database = client['IOT']
-    devices = database['devices']
-    return devices
+elastic = Elasticsearch([{'host': 'localhost', 'port': 9200}])
