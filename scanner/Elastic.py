283815ee368c333d27ccaa38f4eefbd4295f86ae
intrackeable <olesh_@hotmail.com>
2021-12-23 17:44:37 -0300

    Set connection with ElasticsearchDB

--- scanner/Elastic.py
+++ scanner/Elastic.py
@@ -1,23 +1,25 @@
 from datetime import datetime
-from Geolocation import get_data
 from loguru import logger
-from elasticsearch import helpers
-
+from var import CITY, ASN
 import geoip2.database
 
-def create_document(ip, ports, services, banners, hostname, image,default,connection,vnc):
+def create_document(ip, ports_dict, banners, hostname, image,default,connection,vnc):
     try:
-        with geoip2.database.Reader("../geoip2/CITY/GeoLite2-City.mmdb") as geo_reader:
+        keys = list(ports_dict.keys())
+        values = list(ports_dict.values())
+
+        with geoip2.database.Reader(CITY) as geo_reader:
             response = geo_reader.city(ip)
-        with geoip2.database.Reader("../geoip2/ASN/GeoLite2-ASN.mmdb") as asn_reader:        
+        with geoip2.database.Reader(ASN) as asn_reader:        
             resp = asn_reader.asn(ip)
+            
             col = {
                 "ip": ip,
                 "org": resp.autonomous_system_organization,
                 "asn": resp.autonomous_system_number,
                 "banners": banners,
-                "services": services,
-                "ports": ports,
+                "services": keys,
+                "ports": values,
                 "hostname": hostname,
                 "country": response.country.name,
                 "city": response.city.name,
@@ -30,7 +32,7 @@ def create_document(ip, ports, services, banners, hostname, image,default,connec
                 "anonymous_login": default,
                 "date": datetime.now().strftime("%d/%m/%Y %H:%M")
             }
-            logger.success("{} | {} | {} | {} | {}".format(ip,services,response.country.iso_code,response.country.name,response.city.name))
+            logger.success("{} | {} | {} | {}".format(ip,keys,response.country.name,response.city.name))
         doc = {k:v for k,v in col.items() if v is not None}
         
         connection.index(index="devices", document=doc)
